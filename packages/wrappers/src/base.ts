c24875cb17ab60329df7ff1210afad1dbfb4a7b4
Matteo Colombo <158463196+matteobross@users.noreply.github.com>
2025-09-05 12:29:35 +0200

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -176,7 +176,7 @@ export class BaseWrapper {
     );
 
     let response = useProxy
-      ? uFetch(url, {
+      ? Fetch(url, {
           dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
           method: 'GET',
           headers: this.headers,
