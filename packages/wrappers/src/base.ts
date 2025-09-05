d1ae43b926e0deb8cb5c2dfe9800bba39def5c65
Matteo Colombo <158463196+matteobross@users.noreply.github.com>
2025-09-05 12:31:03 +0200

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -177,7 +177,7 @@ export class BaseWrapper {
 
     let response = useProxy
       ? Fetch(url, {
-          dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
+      
           method: 'GET',
           headers: this.headers,
           signal: AbortSignal.timeout(this.indexerTimeout),
